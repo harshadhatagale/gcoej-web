cf0fa84d378a653a10ee45f9f0116ac3f755bd1a
harshadhatagale <harshadhatagale6@gmail.com>
2024-11-05 20:05:15 +0530

    tab view

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
+import KeyPeoplesSection from "@/components/KeyPeoplesSection";
 import Tabs from "@/components/Tabs";
 import Image from "next/image";
 export default function Home() {
-  
+
   return (
     <>
-    <section className="w-full bg-gray-300 h-72 my-5">
+      <section className="w-full bg-gray-300 h-72 my-5">
 
-    </section>
-    <Tabs/>
+      </section>
+      <KeyPeoplesSection />
+      <Tabs />
     </>
   );
 }
--- src/components/KeyPeoplesSection.jsx
+++ src/components/KeyPeoplesSection.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
-
+import Image from 'next/image'
+import { Poppins } from 'next/font/google'
+import { Roboto } from 'next/font/google'
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+})
+const roboto = Roboto({
+    subsets: ["latin"],
+    weight:["100", "300", "400", "500","700","900"]
+})
 export default function KeyPeoplesSection() {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div className='my-6'>
+            <h2 className={`${poppins.className} my-5 text-3xl tracking-wide font-bold`}>Key Peoples:</h2>
+            <div className='p-4 flex space-x-5 my-4 w-full border border-gray-400 shadow-md md:w-[500px] lg:w-[600px] rounded-md'>
+                <Image className='rounded-md' src={"/key1.jpeg"} width={100} height={100} />
+                <div className={`flex flex-col space-y-3`}>
+                    <p className='text-lg font-bold'>Shri. V. C. Rastogi</p>
+                    <p>Hon. Principal Secretary, Higher & Technical Education, Maharashtra</p>
+                </div>
+            </div>
+            <div className='p-4 flex space-x-5 my-4 w-full border border-gray-400 shadow-md md:w-[500px] lg:w-[600px] rounded-md'>
+                <Image className='rounded-md' src={"/key1.jpeg"} width={100} height={100} />
+                <div className={`flex flex-col space-y-3`}>
+                    <p className='text-lg font-bold'>Dr. Vinod Mohitkari</p>
+                    <p>Hon. Director, Directorate of Technical Education,</p>
+                </div>
+            </div>
+            <div className='p-4 flex space-x-5 my-4 w-full border border-gray-400 shadow-md md:w-[500px] lg:w-[600px] rounded-md'>
+                <Image className='rounded-md' src={"/key3.jpeg"} width={100} height={100} />
+                <div className={`flex flex-col space-y-3`}>
+                    <p className='text-lg font-bold'>Dr. Suhas S. Gajre</p>
+                    <p>Hon. Principal, GCoE, Jalgaon</p>
+                </div>
+            </div>
+        </div>
+    )
 }
