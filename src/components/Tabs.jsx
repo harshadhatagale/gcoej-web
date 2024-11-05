04e1637c90eb99883f9c9f7eed22b9ed3a6a9994
harshadhatagale <harshadhatagale6@gmail.com>
2024-11-05 18:55:58 +0530

    tab view

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Tabs() {
   return (
     <div className='w-full overflow-hidden'>
-        <div className='lg:w-[800px] min-w-[300px] min:md:w-[500px] flex p-2 justify-start items-center bg-gray-300 min-h-12 rounded-md flex-wrap'>
+        <div className='lg:w-[800px] min-w-[300px] min:md:w-[500px] flex p-2 justify-start items-center bg-gray-200 min-h-12 rounded-md flex-wrap'>
             <div className='h-full flex-1 py-2 px-4 justify-center items-center flex cursor-pointer hover:bg-blue-600 hover:text-white rounded-md'>News</div>
             <div className='h-full flex-1 py-2 px-4 justify-center items-center flex cursor-pointer hover:bg-blue-600 hover:text-white rounded-md'>Notices</div>
             <div className='h-full flex-1 py-2 px-4 justify-center items-center flex cursor-pointer hover:bg-blue-600 hover:text-white rounded-md'>Tenders</div>
