aa8ee707cf8e7bbc4fa0524e33f05afbfdb670bb
harshadhatagale <harshadhatagale6@gmail.com>
2024-11-08 19:15:44 +0530

    Footer

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,37 @@
 import Heading from '@/components/UI/Heading1'
 import React from 'react'
 
+
 export default function page() {
     return (
         <>
-        <Heading content={"About Institute"} />
+            <Heading content={"About Institute"} />
+            <table class="table-auto">
+                <thead>
+                    <tr>
+                        <th>Song</th>
+                        <th>Artist</th>
+                        <th>Year</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                        <td>Malcolm Lockyer</td>
+                        <td>1961</td>
+                    </tr>
+                    <tr>
+                        <td>Witchy Woman</td>
+                        <td>The Eagles</td>
+                        <td>1972</td>
+                    </tr>
+                    <tr>
+                        <td>Shining Star</td>
+                        <td>Earth, Wind, and Fire</td>
+                        <td>1975</td>
+                    </tr>
+                </tbody>
+            </table>
         </>
     )
 }
--- src/components/UI/Heading1.jsx
+++ src/components/UI/Heading1.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { Roboto } from 'next/font/google'
+const roboto = Roboto({
+    subsets: ["latin"],
+    weight: ["100", "300", "400", "500", "700", "900"],
+});
 export default function Heading({content}) {
   return (
     <div className='text-2xl w-full bg-cover bg-center bg-opacity-40 bg-[url("/header-bg.jpg")] h-16 flex capitalize tracking-wide font-bold text-blue-900 rounded-sm px-2 justify-start items-center'>
-      <h2 className='text-shadow-lg'>
+      <h2 className={`text-shadow-lg font-extrabold ${roboto.className}`}>
       {content}
     </h2>
     </div>
